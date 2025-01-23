9435fee041cffe4795266c2cc79ba44a2bbba9a2
Asuritys-Inc <75674175+AntiParty@users.noreply.github.com>
2025-01-23 04:05:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ async function updateStats() {
       const response = await fetch('./stats.json');
       const data = await response.json();
 
+      // Extract the necessary statistics
+      const subscriberCount = data.items[0].statistics.subscriberCount;
+      const viewCount = data.items[0].statistics.viewCount;
+
+      // Update the HTML with the data
       document.querySelector('.stats').innerHTML = `
-          ${data}+ <span class="testing-red"><i class="fab fa-youtube"></i> YouTube</span><br />
-          15,000+ <span class="testing-blue"><i class="fa-brands fa-tiktok"></i> TikTok</span><br />
+          ${viewCount} <span class="testing-red"><i class="fab fa-youtube"></i> YouTube</span><br />
+          ${subscriberCount} <span class="testing-blue"><i class="fa-brands fa-tiktok"></i> TikTok</span><br />
           Business Inquiries: <a href="mailto:antiparty99@gmail.com" class="email-link">antiparty99@gmail.com</a>
       `;
   } catch (error) {
