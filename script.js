2b0b044263eec0c9cafca79e61d3fdd3b5dea84e
Asuritys-Inc <75674175+AntiParty@users.noreply.github.com>
2025-01-23 04:12:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ async function updateStats() {
       const response = await fetch('./stats.json');
       const data = await response.json();
 
-      const subcount = data.items[0].statistics.subscriberCount;
+      // Ensure the subscriber count is an integer
+      const subcount = parseInt(data.items[0].statistics.subscriberCount, 10);
 
+      // Update the HTML with the subscriber count
       document.querySelector('.stats').innerHTML = `
           ${subcount}+ <span class="testing-red"><i class="fab fa-youtube"></i> YouTube</span><br />
           15,000+ <span class="testing-blue"><i class="fa-brands fa-tiktok"></i> TikTok</span><br />
